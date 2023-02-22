2d1217572b5640f5213879f0590d84b13ae9e846
am3rco <am3rco@gmail.com>
2023-02-22 16:39:23 -0300

    Cliente

--- src/Cliente/TestaCliente.java
+++ src/Cliente/TestaCliente.java
@@ -6,37 +6,15 @@ public class TestaCliente {
 
 	public static void main(String[] args) {
 		
-		// Instanciando um objeto da Classe Cliente
-		Cliente cliente = new Cliente("Julio Da Gaita", "julinho@juju.com", "Homem", "(11)-999999999", 18);
+		 // Instanciando dois objetos da Classe Cliente
+        Cliente cliente1 = new Cliente("João da Silva", "Rua A, 123", "(11) 1234-5678", "joao.silva@email.com", 35);
+        Cliente cliente2 = new Cliente("Maria Oliveira", "Avenida B, 456", "(11) 2345-6789", "maria.oliveira@email.com", 25);
+        
+        // Exibindo os dados dos objetos Cliente utilizando o método visualizar()
+        cliente1.visualizar();
+        System.out.println();
+        cliente2.visualizar();
 		
-		Scanner leia = new Scanner(System.in);
-		
-		// Declara as Variaveis
-		String n, g, e, t;
-		int i;
-		
-		System.out.println("\nInsira o seu nome: ");
-		n = leia.nextLine();
-		cliente.setNome(n);
-		
-		System.out.println("\nInsira o seu email: ");
-		e = leia.nextLine();
-		cliente.setEmail(e);
-		
-		System.out.println("\nInsira o seu genero: ");
-		g = leia.nextLine();
-		cliente.setGenero(g);
-		
-		System.out.println("\nInsira o seu telefone: ");
-		t = leia.nextLine();
-		cliente.setTelefone(t);
-		
-		System.out.println("\nInsira a sua idade: ");
-		i = leia.nextInt();
-		cliente.setIdade(i);
-		
-		// Imprime a visualização do Cliente
-		cliente.visualizar();
 		
 		
 	}
